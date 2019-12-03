e3d4b738810e8b61fbcecebf44ae5aa3cfed8ff7
hanseokhyeon <hanseokhyeon@hanseokhyeon-MacBookPro.local>
2019-12-03 19:37:05 +0900

    1965_box complete

--- baekjoon_20191202/1965_box.py
+++ baekjoon_20191202/1965_box.py
@@ -1,22 +1,28 @@
 if __name__ == '__main__':
     n = int(input())
     boxes = list(map(int, input().split()))
+    boxes.insert(0, 0)
 
-    max_count = -1
+    a = list(zip(boxes, range(len(boxes))))
 
-    for i, data in enumerate(boxes):
-        if i == 0: continue
-        count = 0
-        before = -1
-        for tmp in boxes[0:i]:
-            if data > tmp and tmp > before:
-                count += 1
-            before = tmp
-        if count > max_count:
-            max_count = count
+    d = [0]
+    for box, i in a:
+        if i == 0:
+            continue
 
+        a_before = a[:i]
 
-    box = zip(boxes, range(0, len(boxes)))
-    idx = max(box)[1]
+        a_small = list(map(lambda x: x if x[0] < box else None, a_before))
+        while None in a_small:
+            a_small.remove(None)
 
-    pass
+        a_small_idx = list(map(lambda x: x[1], a_small))
+
+        d_max = 0
+        for k in a_small_idx:
+            if d[k] > d_max:
+                d_max = d[k]
+
+        d.append(d_max+1)
+
+    print(max(d))
