023d5f13c6c772f31ce7483ecf3fc0edd103b13e
HanSeokhyeon <sah0322@naver.com>
2019-12-08 12:20:46 +0900

    rename dir

---
+++ 20191206_programmers_exhaustive_search/number_baseball.py
@@ -0,0 +1,26 @@
+count = 0
+
+def check_case(baseball, nums):
+    global count
+    if len(baseball) == 0:
+        count += 1
+        return
+    for i, case in enumerate(baseball):
+        pass
+
+def strike(n_try, n_strike):
+    for
+
+
+
+def solution(baseball):
+    nums = [list(range(1, 10)) for _ in range(3)]
+    for i, case in enumerate(baseball):
+        pass
+
+
+    answer = 0
+    return answer
+
+if __name__ == '__main__':
+    print(solution([[123, 1, 1], [356, 1, 0], [327, 2, 0], [489, 0, 1]]))
