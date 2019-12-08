79586e4e881d39b5f88908d75cae743251485c34
HanSeokhyeon <sah0322@naver.com>
2019-12-08 12:40:27 +0900

    carpet

---
+++ 20191206_programmers_exhaustive_search/carpet.py
@@ -0,0 +1,22 @@
+def check_carpet(brown, red, w, h):
+    if w*2 + h*2 - 4 == brown:
+        if (w-2)*(h-2) == red:
+            return True
+    return False
+
+
+def solution(brown, red):
+    answer = []
+    n_carpet = brown + red
+    for i in range(1, int(n_carpet**0.5)+1):
+        if n_carpet % i == 0:
+            if check_carpet(brown, red, n_carpet // i, i):
+                answer.append(n_carpet//i)
+                answer.append(i)
+    return answer
+
+
+if __name__ == '__main__':
+    print(solution(10, 2))
+    print(solution(8, 1))
+    print(solution(24, 24))
