3ba6cd2fd3fbd845ae49232b77dbc6f699c93b1e
20odeebh <31841180+20odeebh@users.noreply.github.com>
2019-12-11 22:06:31 +0900

    Update 카펫.cpp

--- 20191206 HW/카펫.cpp
+++ 20191206 HW/카펫.cpp
@@ -1,1 +1,19 @@
+#include <string>
+#include <vector>
 
+using namespace std;
+
+vector<int> solution(int brown, int red) {
+    vector<int> answer;
+    int length = brown + red;
+    for (int i = 3; i <length; i++) {
+        if (length % i == 0) {
+            int  j = length / i;
+            if ((((2 * i) + (2 * j) - 4) == brown) && length - brown == red) {
+                answer.push_back(j); answer.push_back(i);
+                break;
+            }
+        }
+    }
+    return answer;
+}
