d60ead625f77183f3e81244a42bb4a22b0b1a79c
20odeebh <31841180+20odeebh@users.noreply.github.com>
2019-12-11 21:33:39 +0900

    Update 네트워크.cpp

--- 20191209/네트워크.cpp
+++ 20191209/네트워크.cpp
@@ -1,1 +1,28 @@
-
+#include <string>
+#include <vector>
+using namespace std;
+ 
+bool visited[201];
+ 
+void DFS(int first, vector<vector<int>>&computers, int n) {
+    visit[first] = true;
+    
+    for (int i = 0; i < n; i++) {
+        if ((computers[start][i] == 1)&&(!visited[i])  ) {
+            dfs(i, computers, n);
+        }
+      }
+  }
+ 
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            DFS(i, computers, n);
+            answer++;
+        }
+      }
+    
+    return answer;
+}
