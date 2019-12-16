d3d3ea689aeed655ec2199ae5c5cfb9ae4f8ae91
jooyoung0525 <pjuy0525@naver.com>
2019-12-16 11:49:17 +0900

    Update Baekjoon_10026적록색약.cpp

--- Study_4D(HW)/Baekjoon_10026적록색약.cpp
+++ Study_4D(HW)/Baekjoon_10026적록색약.cpp
@@ -14,7 +14,7 @@ char map[110][110] = { 0 };
 bool check[110][110] = { 0 };
 int dx[] = { -1,1,0,0 };
 int dy[] = { 0,0,-1,1 };
-bool second = false;
+bool second = false; //정상과 적록색약 dfs돌릴때 구분해주기 위해
 vector<int>result;
 
 void input();
@@ -23,8 +23,8 @@ void Find();
 int main()
 {
 	input();
-	Find();
-	Find();
+	Find(); //정상인사람 dfs
+	Find(); //적록색약 dfs
 
 	for (int i = 0; i < 2; i++)
 	{
@@ -42,29 +42,31 @@ void DFS(int curx,int cury, char now)
 		int ny = cury + dy[i];
 
 		if (nx < 0 || ny < 0 || nx >= N || ny >= N)continue;
-		if (second == false) 
+		
+		if (second == false)  // 정상인사람 dfs
 		{
-			if (check[nx][ny] == false && now == map[nx][ny])
+			if (check[nx][ny] == false && now == map[nx][ny]) //방문한적 없고, 다음구역이 같은색이면
 			{
 				check[nx][ny] = true;
 				DFS(nx, ny, map[nx][ny]);
 			}
 		}
-		if (second == true)
+		
+		if (second == true) //적록색약인사람 dfs
 		{
-			if (check[nx][ny] == false)
+			if (check[nx][ny] == false) //방문한적 없고,
 			{
-				if (now == 'R' && (map[nx][ny] == 'G' || map[nx][ny] == 'R')) {
-					check[nx][ny] = true;
-					DFS(nx, ny, map[nx][ny]);
+				if (now == 'R' && (map[nx][ny] == 'G' || map[nx][ny] == 'R')) { //현재구역이 R일때, 다음구역이 G나 R이면
+					check[nx][ny] = true;  //방문체크 후,
+					DFS(nx, ny, map[nx][ny]); //DFS
 				}
-				if (now == 'G' && (map[nx][ny] == 'G' || map[nx][ny] == 'R')) {
-					check[nx][ny] = true;
-					DFS(nx, ny, map[nx][ny]);
+				if (now == 'G' && (map[nx][ny] == 'G' || map[nx][ny] == 'R')) { //현재구역이 G일때, 다음구역이 G나 R이면
+					check[nx][ny] = true;   //방문체크 후,
+					DFS(nx, ny, map[nx][ny]); //DFS
 				}
-				if (now == 'B' && map[nx][ny] == 'B') {
-					check[nx][ny] = true;
-					DFS(nx, ny, map[nx][ny]);
+				if (now == 'B' && map[nx][ny] == 'B') { //현재구역이 B일때, 다음구역이 B이면
+					check[nx][ny] = true;   //방문체크 후,
+					DFS(nx, ny, map[nx][ny]); //DFS
 				}
 			}
 		}
@@ -85,12 +87,13 @@ void input()
 void Find()
 {
 	int cnt = 0;
-	memset(check, 0, sizeof(check));
+	memset(check, 0, sizeof(check)); //check배열 초기화
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < N; j++)
 		{
-			if (check[i][j] == 0) {
+			if (check[i][j] == 0) { //방문한적 없으면 dfs
+				
 				DFS(i, j, map[i][j]);
 				cnt++;
 			}
@@ -98,5 +101,5 @@ void Find()
 	}
 
 	result.push_back(cnt);
-	second = true;
+	second = true; //적록색약일때의 dfs돌리기 위해
 }
