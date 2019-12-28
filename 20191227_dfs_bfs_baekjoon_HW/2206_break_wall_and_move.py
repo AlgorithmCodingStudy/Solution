f9a389ddb60dcc9700d81fe281b8197500cd0913
HanSeokhyeon <sah0322@naver.com>
2019-12-28 14:20:58 +0900

    update

--- 20191227_dfs_bfs_baekjoon_HW/2206_break_wall_and_move.py
+++ 20191227_dfs_bfs_baekjoon_HW/2206_break_wall_and_move.py
@@ -10,5 +10,36 @@
 
 알고리즘: BFS
 
-1.
+1. 일단 벽을 부수지 않고 최단경로를 구해보자.
 """
+from collections import deque
+import sys
+read = sys.stdin.readline
+n, m = map(int, read().strip().split())
+matrix = [list(map(int, read().strip())) for _ in range(n)]
+
+
+def bfs():
+    q = deque()
+    q.append((0, 0))
+    visit = {}
+    visit[(0, 0)] = True
+    length = 0
+
+    while q:
+        x, y = q.popleft()
+        if x == n-1 and y == m-1:
+            return length
+        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            nx, ny = x + dx, y + dy
+            if not (0 <= nx < n and 0 <= ny < m):
+                continue
+            if (nx, ny) not in visit and matrix[nx][ny] == 0:
+                q.append((nx, ny))
+                visit[(nx, ny)] = True
+        length += 1
+
+
+result = bfs()
+
+pass
