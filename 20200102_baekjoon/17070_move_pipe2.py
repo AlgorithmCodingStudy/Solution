02d69145d9d8127d1a4ebafc07386ef9789bea75
HanSeokhyeon <sah0322@naver.com>
2020-01-02 15:26:07 +0900

    HW

--- 20200102_baekjoon/17070_move_pipe2.py
+++ 20200102_baekjoon/17070_move_pipe2.py
@@ -27,79 +27,52 @@ dxy = [(0, 1), (1, 1), (1, 0)]  # 우, 우하, 하
 
 def dfs():
     cnt = 0
-    stack = [((0, 0), (0, 1))]
-    visit = {}
+    stack = [((0, 1), 0)]
 
     def check_range(x, y):
         return 0 <= x < n and 0 <= y < n
 
     def check_horizontal(x, y):
         nx, ny = x, y + 1  # 가로
-        if check_range(nx, ny) and ((x, y), (nx, ny)) not in visit:
+        if check_range(nx, ny):
             if house[nx][ny] == 0:
-                return (x, y), (nx, ny)  # 가로 추가
+                return [((nx, ny), 0)]  # 가로 추가
+        return []
 
     def check_vertical(x, y):
         nx, ny = x + 1, y
-        if check_range(nx, ny) and ((x, y), (nx, ny)) not in visit:
+        if check_range(nx, ny):
             if house[nx][ny] == 0:
-                return (x, y), (nx, ny)  # 세로 추가
+                return [((nx, ny), 1)]  # 세로 추가
+        return []
 
     def check_diagonal(x, y):
-        diagonal = True
-        for dx, dy in dxy:
-            nx, ny = x + dx, y + dy
-            if check_range(nx, ny):
-                if house[nx][ny] == 1:
-                    diagonal = False
-        if check_range(x + 1, y + 1):
-            if diagonal and ((x, y), (x + 1, y + 1)) not in visit:
-                return (x, y), (x + 1, y + 1)  # 대각선 추가
-
-    def nxt_value(state):
-        nxt_list = []
-        # state 0: 가로
-        # state 1: 세로
-        # state 2: 대각선
-        if state == 0:
-            nxt_list.append(check_horizontal(*right))
-            nxt_list.append(check_diagonal(*right))
-
-        elif state == 1:
-            nxt_list.append(check_vertical(*right))
-            nxt_list.append(check_diagonal(*right))
-
-        else:
-            nxt_list.append(check_horizontal(*right))
-            nxt_list.append(check_vertical(*right))
-            nxt_list.append(check_diagonal(*right))
-
-        for nxt in nxt_list:
-            if not nxt:
-                nxt_list.remove(None)
-        if nxt_list:
-            stack.extend(nxt_list)
-        else:
-            visit[(left, right)] = True
+        nx, ny = x + 1, y + 1
+        if check_range(nx, ny):
+            if house[x+1][y] == 0 and house[x][y+1] == 0 and house[x+1][y+1] == 0:
+                return [((nx, ny), 2)]  # 대각선 추가
+        return []
 
     while stack:
-        tmp = stack.pop()
-        if tmp == None: continue
-        else: left, right = tmp
+        right, direction = stack.pop()
         if right == (n-1, n-1):
             cnt += 1
 
-        if right[0] - left[0] == 1:
-            if right[1] - left[1] == 1:
-                # 대각선
-                nxt_value(2)
-            else:
-                # 세로
-                nxt_value(1)
+        # direction 0: 가로
+        # direction 1: 세로
+        # direction 2: 대각선
+        if direction == 0:
+            stack.extend(check_horizontal(*right))
+            stack.extend(check_diagonal(*right))
+
+        elif direction == 1:
+            stack.extend(check_vertical(*right))
+            stack.extend(check_diagonal(*right))
+
         else:
-            if right[1] - left[1] == 1:
-                # 로
-                nxt_value(0)
+            stack.extend(check_horizontal(*right))
+            stack.extend(check_vertical(*right))
+            stack.extend(check_diagonal(*right))
 
     return cnt
 
