1992e8d1bfe5d0c334ea2175698257b019ca7b9d
hanseokhyeon <sah0322@naver.com>
2020-01-23 12:57:46 +0900

    12:33

--- 20200123_sw_expert_academy/5656_breaking_blocks.py
+++ 20200123_sw_expert_academy/5656_breaking_blocks.py
@@ -31,59 +31,62 @@ def dfs(now_blocks, n_cnt):
         def range_check(x_, y_):  # 범위 체크
             return 0 <= x_ < w and 0 <= y_ < h
 
+        def break_(n_block, x, y):
+            breaking_blocks[x][y] = 0  # 현 위치 블럭은 부쉈으니 0으로
+
+            for delta in range(-(n_block-1), n_block):  # ex) delta: -3 ~ 3
+                for nx, ny in [(x+delta, y), (x, y+delta)]:  # 세로와 가로
+                    if range_check(nx, ny):  # 범위 안에 있으면
+                        num = breaking_blocks[nx][ny]  # 현재 블럭 넘버
+                        if num > 1:  # 1보다 크면 현재 위치 기준으로 다시 break_ 호출
+                            break_(num, nx, ny)
+                        elif num == 1:  # 1이면 부수고 끝
+                            breaking_blocks[nx][ny] = 0
+
         if 0 not in breaking_blocks[idx_x]:  # 현재 라인에 0이 없으면 제일 위 블럭을 start로
             idx_y = h-1
         else:  # 0의 위치 - 1을 start로
             idx_y = breaking_blocks[idx_x].index(0) - 1
 
-        def break_(n_block, x, y):
-            breaking_blocks[x][y] = 0
-
-            for a in range(-(n_block-1), n_block):
-                nx1, ny1, nx2, ny2 = x+a, y, x, y+a
-
-                if range_check(nx1, ny1):
-                    num = breaking_blocks[nx1][ny1]
-                    if num > 1:
-                        break_(num, nx1, ny1)
-                    elif num == 1:
-                        breaking_blocks[nx1][ny1] = 0
-
-                if range_check(nx2, ny2):
-                    num = breaking_blocks[nx2][ny2]
-                    if num > 1:
-                        break_(num, nx2, ny2)
-                    elif num == 1:
-                        breaking_blocks[nx2][ny2] = 0
-
         break_(breaking_blocks[idx_x][idx_y], idx_x, idx_y)
 
         return breaking_blocks
 
-    def gravity():
-        for line in new_blocks:
-            n_zero = line.count(0)
+    def gravity():  # 중력 적용
+        """
+        11100110
+        10110001
+        """
+        for line in new_blocks:  # line을 하나씩 가져와서
+            n_zero = line.count(0)  # 0의 개수를 파악하고
             for _ in range(n_zero):
-                line.remove(0)
-
+                line.remove(0)  # 모든 0을 지운다.
+        """
+        11111
+        1111
+        """
         for line in new_blocks:
-            line += [0] * (h - len(line))
+            line += [0] * (h - len(line))  # 지운 것 뒤에 height까지 0으로 채운다.
+        """
+        11111000
+        11110000
+        """
 
     global min_remains
-    if n_cnt == n:
+    if n_cnt == n:  # n번의 시도를 모두 사용했으면
         count_0 = 0
-        for line_ in now_blocks:
+        for line_ in now_blocks:  # 0의 개수를 세고
             count_0 += line_.count(0)
-        count_non_zero = w*h - count_0
-        if count_non_zero < min_remains:
+        count_non_zero = w*h - count_0  # 전체 넓이에서 0의 개수를 빼서 블럭수를 구한다.
+        if count_non_zero < min_remains:  # 최소값과 비교
             min_remains = count_non_zero
         return
 
     else:
-        for i in range(w):
-            new_blocks = break_block(deepcopy(now_blocks), i)
-            gravity()
-            dfs(new_blocks, n_cnt+1)
+        for i in range(w):  # 블럭을 부술수 있는 모든 위치에서 부수기 시도
+            new_blocks = break_block(deepcopy(now_blocks), i)  # i위치 블럭 부수기
+            gravity()  # 중력 적용
+            dfs(new_blocks, n_cnt+1)  # 다음 시도를 위해 부순 다음 블럭들을 가지고 재귀호출
 
 
 T = int(input())
@@ -94,7 +97,7 @@ for test_case in range(1, T + 1):
     n, w, h = map(int, input().split())
     blocks = [list(map(int, input().split())) for _ in range(h)]
 
-    blocks = [list(reversed(x)) for x in zip(*blocks)]
+    blocks = [list(reversed(x)) for x in zip(*blocks)]  # 중력 적용을 쉽게 하기 위해 transpose
 
     min_remains = 10000000
     dfs(blocks, 0)
