cb073066845cd748ce849bf7f6223456bb00a396
hanseokhyeon <sah0322@naver.com>
2020-01-23 12:43:34 +0900

    12:33

--- 20200123_sw_expert_academy/5656_breaking_blocks.py
+++ 20200123_sw_expert_academy/5656_breaking_blocks.py
@@ -19,31 +19,36 @@
         1. 현재 위치 벽돌 부수고 부수면서 다시 재귀?
         2. 다부수고 나면 중력 적용
 """
+
 from copy import deepcopy
 import sys
 sys.stdin = open("input2.txt", "r")
 sys.setrecursionlimit(100000)
 
 
-def dfs(now_blocks, cnt):
-    def break_block(breaking_blocks, idx):
-        def range_check(x_, y_):
+def dfs(now_blocks, n_cnt):
+    def break_block(breaking_blocks, idx_x):
+        def range_check(x_, y_):  # 범위 체크
             return 0 <= x_ < w and 0 <= y_ < h
-        if 0 not in breaking_blocks[idx]:
-            start = h-1
-        else:
-            start = breaking_blocks[idx].index(0) - 1
+
+        if 0 not in breaking_blocks[idx_x]:  # 현재 라인에 0이 없으면 제일 위 블럭을 start로
+            idx_y = h-1
+        else:  # 0의 위치 - 1을 start로
+            idx_y = breaking_blocks[idx_x].index(0) - 1
 
         def break_(n_block, x, y):
             breaking_blocks[x][y] = 0
+
             for a in range(-(n_block-1), n_block):
                 nx1, ny1, nx2, ny2 = x+a, y, x, y+a
+
                 if range_check(nx1, ny1):
                     num = breaking_blocks[nx1][ny1]
                     if num > 1:
                         break_(num, nx1, ny1)
                     elif num == 1:
                         breaking_blocks[nx1][ny1] = 0
+
                 if range_check(nx2, ny2):
                     num = breaking_blocks[nx2][ny2]
                     if num > 1:
@@ -51,33 +56,34 @@ def dfs(now_blocks, cnt):
                     elif num == 1:
                         breaking_blocks[nx2][ny2] = 0
 
-        break_(breaking_blocks[idx][start], idx, start)
+        break_(breaking_blocks[idx_x][idx_y], idx_x, idx_y)
 
         return breaking_blocks
 
-    def gravity(set_blocks):
-        for line in set_blocks:
-            n_iter = line.count(0)
-            for _ in range(n_iter):
+    def gravity():
+        for line in new_blocks:
+            n_zero = line.count(0)
+            for _ in range(n_zero):
                 line.remove(0)
 
-        for line in set_blocks:
-            line += [0]*(h - len(line))
+        for line in new_blocks:
+            line += [0] * (h - len(line))
 
     global min_remains
-    if cnt == n:
+    if n_cnt == n:
         count_0 = 0
-        for line in now_blocks:
-            count_0 += line.count(0)
-        count_1 = w*h - count_0
-        if count_1 < min_remains:
-            min_remains = count_1
+        for line_ in now_blocks:
+            count_0 += line_.count(0)
+        count_non_zero = w*h - count_0
+        if count_non_zero < min_remains:
+            min_remains = count_non_zero
+        return
+
     else:
         for i in range(w):
             new_blocks = break_block(deepcopy(now_blocks), i)
-            gravity(new_blocks)
-            dfs(new_blocks, cnt+1)
-            pass
+            gravity()
+            dfs(new_blocks, n_cnt+1)
 
 
 T = int(input())
@@ -87,10 +93,11 @@ for test_case in range(1, T + 1):
 
     n, w, h = map(int, input().split())
     blocks = [list(map(int, input().split())) for _ in range(h)]
+
     blocks = [list(reversed(x)) for x in zip(*blocks)]
+
     min_remains = 10000000
     dfs(blocks, 0)
     print("#{} {}".format(test_case, min_remains))
 
     # ///////////////////////////////////////////////////////////////////////////////////
-
