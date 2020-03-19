ea5599de7fdb7eb00248c19568c797f3937ec91a
hanseokhyeon <sah0322@naver.com>
2020-03-19 14:03:12 +0900

    update

--- 20200318_baekjoon_HW/2240_plum_tree.py
+++ 20200318_baekjoon_HW/2240_plum_tree.py
@@ -10,25 +10,22 @@
 2. n=0 0, 1
 3. dp[n, m] = max(dp[n-1, m], dp[n-1, m-1]) + tree[n, m]
 """
-from itertools import chain
 import sys
 read = sys.stdin.readline
 
 t, w = map(int, read().strip().split())
-tree = [[0, 0]]
+tree = [0]
 for _ in range(t):
-    num = [0, 0]
-    num[int(read().strip()) - 1] += 1
-    tree.append(num)
+    tree.append(int(read().strip())-1)
 
 dp = [[0]*(w+1) for _ in range(t+1)]
 for i in range(1, t+1):
-    dp[i][0] = dp[i - 1][0] + tree[i][0]
-    for j in range(1, min(i, w+1)):
-        if tree[i][j%2] == 1 and j%2==0:
+    dp[i][0] = dp[i - 1][0] + (tree[i]==0)
+    for j in range(1, min(i+1, w+1)):
+        if tree[i] == 0 and j%2==0:
             dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + 1
-        elif tree[i][j%2] == 1 and j%2==1:
+        elif tree[i] == 1 and j%2==1:
             dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + 1
         else:
             dp[i][j] = max(dp[i-1][j-1], dp[i-1][j])
-print(max(chain(*dp)))
+print(max(dp[-1]))
