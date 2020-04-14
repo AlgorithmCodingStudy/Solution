01e6db9842775dbc8c02a2cd5283172b73d6fa70
jooyoung0525 <pjuy0525@naver.com>
2020-04-14 11:31:36 +0900

    Update 백준11726_2*n타일링.cpp

--- 202004_셋째주HW/백준11726_2*n타일링.cpp
+++ 202004_셋째주HW/백준11726_2*n타일링.cpp
@@ -17,7 +17,8 @@ int main()
 
 	for (int i = 3; i <= N; i++)
 	{
-		memo[i] = (memo[i - 1] + memo[i - 2])%10007;
+		memo[i] = (memo[i - 1] + memo[i - 2]) % 10007; //계산하면서 오버플로우 발생할 수 있어서 마지막에 나머지를 나눠주는게 아니라
+		                                               //계산하면서 나머지를 계속 나눠줘야함
 	}
 
 	cout << memo[N]<< endl;
