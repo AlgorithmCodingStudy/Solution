a23acd2097053ad85b7c5731b6add30f9fe4c91c
jooyoung0525 <pjuy0525@naver.com>
2020-04-15 21:50:05 +0900

    Create 백준5587_카드캡처.cpp

---
+++ 20200415/백준5587_카드캡처.cpp
@@ -0,0 +1,161 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+
+using namespace std;
+
+int n;
+bool check[220];
+int Sang[110];
+int Geun[110];
+int Play = -1;
+
+void pick_sang();
+void pick_geun();
+bool play_stop();
+
+int main()
+{
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> Sang[i];
+		check[Sang[i]] = true;
+	}
+
+	int cnt = 0;
+	for (int i = 1; i <= 2*n; i++)
+	{
+		if (!check[i]) {
+			Geun[cnt] = i;
+			cnt++;
+		}
+	}
+
+	sort(Sang, Sang + n);
+	sort(Geun, Geun + n);
+
+
+	while (!play_stop())
+	{
+	
+		pick_sang();
+		pick_geun();
+	}
+
+
+	int Scnt = 0;
+	int Gcnt = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (Sang[i] > 0) Scnt++;
+		if (Geun[i] > 0) Gcnt++;
+	}
+
+	cout << Gcnt << endl;
+	cout << Scnt << endl;
+	return 0;
+}
+
+void pick_sang()
+{
+	if (Play == -1)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			if (Sang[i] > 0)
+			{
+				Play=Sang[i];
+				Sang[i] = 0;
+				break;
+			}
+		}
+	}
+	else
+	{
+		bool can = false;
+		int now = 0;
+
+		for (int i = 0; i < n; i++)
+		{
+			if (Sang[i] > 0)
+			{
+				now = Sang[i];
+				if (Play < now) 
+				{
+					Sang[i] = 0;
+					Play=now;
+					can = true;
+					break;
+				}
+			}
+		}
+
+		if (!can)
+		{
+			Play = -1;
+		}
+
+	}
+}
+
+void pick_geun()
+{
+	
+	if (Play == -1)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			if (Geun[i] > 0)
+			{
+				Play=Geun[i];
+				Geun[i] = 0;
+				break;
+			}
+		}
+	}
+	else
+	{
+		bool can = false;
+		int now = 0;
+
+		for (int i = 0; i < n; i++)
+		{
+			if (Geun[i] > 0) {
+				now = Geun[i];
+				if (Play < now)
+				{
+					Geun[i] = 0;
+					Play=now;
+					can = true;
+					break;
+				}
+			}
+		}
+
+		if (!can)
+		{
+			Play = -1;
+		}
+	}
+}
+
+
+
+bool play_stop()
+{
+	int Scnt = 0;
+	int Gcnt = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (Sang[i] > 0) Scnt++;
+		if (Geun[i] > 0) Gcnt++;
+	}
+
+	if (Scnt == 0 || Gcnt == 0) return true;
+
+	return false;
+}
