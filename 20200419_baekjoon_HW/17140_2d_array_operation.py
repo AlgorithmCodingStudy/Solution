76d83981e1202855500395d3638ac436be988c01
hanseokhyeon <sah0322@naver.com>
2020-04-20 13:42:57 +0900

    17140

--- 20200419_baekjoon_HW/17140_2d_array_operation.py
+++ 20200419_baekjoon_HW/17140_2d_array_operation.py
@@ -25,21 +25,24 @@ def operation(now_a, r_or_c):
         for row in now_a:
             cnt = {}
             for v in row:
+                if v == 0:
+                    continue
                 if v in cnt:
                     cnt[v] += 1
                 else:
                     cnt[v] = 1
             new_line = []
-            for key in sorted(cnt, key=lambda x: cnt[x]):
+            for key in sorted(cnt, key=lambda x: (cnt[x], x)):
                 new_line.extend([key, cnt[key]])
             new_a.append(new_line)
             max_length = max(max_length, len(new_line))
 
-        max_length = max_length if max_length <= 100 else 100
         for i, line in enumerate(new_a):
-            new_a[i] = line[:max_length] + [0]*(max_length-len(line))
+            new_a[i] = line + [0]*(max_length-len(line))
+        for i, line in enumerate(new_a):
+            new_a[i] = line[:100]
 
-        return new_a, len(new_a), max_length
+        return new_a, len(new_a), len(new_a[0])
 
     def transpose(a_):
         a_ = [list(line) for line in zip(*a_)]
@@ -60,7 +63,12 @@ def operation(now_a, r_or_c):
 
 time = 0
 max_r, max_c = 3, 3
-while a[r-1][c-1] != k:
+while True:
+    try:
+        if a[r-1][c-1] == k:
+            break
+    except IndexError:
+        pass
     a = operation(a, max_r >= max_c)
     time += 1
     if time > 100:
