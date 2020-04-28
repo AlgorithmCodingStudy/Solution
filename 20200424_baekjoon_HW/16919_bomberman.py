37838dc3ec8d3eedf1b41ba7673466342a32dd52
hanseokhyeon <sah0322@naver.com>
2020-04-28 20:32:47 +0900

    16919 not complete

--- 20200424_baekjoon_HW/16919_bomberman.py
+++ 20200424_baekjoon_HW/16919_bomberman.py
@@ -15,3 +15,54 @@ import sys
 read = sys.stdin.readline
 
 r, c, n = map(int, read().split())
+time = [[-1]*c for _ in range(r)]
+state = []
+for x in range(r):
+    state.append(list(read().strip()))
+    for y in range(c):
+        if state[x][y] == 'O':
+            time[x][y] = 2
+
+
+def put_bomb():
+    for i in range(r):
+        for j in range(c):
+            if time[i][j] == -1:
+                time[i][j] = 3
+                state[i][j] = 'O'
+
+
+def bomb():
+    dxy = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    remove = set()
+    for i in range(r):
+        for j in range(c):
+            if time[i][j] == 0:
+                remove.add((i, j))
+                for dx, dy in dxy:
+                    nx, ny = i+dx, j+dy
+                    if not (0 <= nx < r and 0 <= ny < c):
+                        continue
+                    remove.add((nx, ny))
+
+    for i, j in remove:
+        time[i][j] = -1
+        state[i][j] = '.'
+
+
+def nxt_time():
+    for i in range(r):
+        for j in range(c):
+            if time[i][j] != -1:
+                time[i][j] -= 1
+
+
+for t in range(1, n):
+    nxt_time()
+    if t % 2:
+        put_bomb()
+    else:
+        bomb()
+
+for line in state:
+    print("".join(line))
